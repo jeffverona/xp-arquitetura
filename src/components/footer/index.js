72fb198a4c0d889a0a66e42999605f9575beca84
jeffverona <jefferson.verona@gmail.com>
2019-06-12 00:34:49 -0300

    [Footer] Component + test :hammer:

---
+++ src/components/footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <hr/>
+
+    <h3>Xp Investimentos</h3>
+
+    <p>Autor: Jefferson Verona | Front-end Developer</p>
+
+    <p>e-mail: <a href="mailto:jefferson.verona@xpi.com.br">jefferson.verona@xpi.com.br</a>.
+    </p>
+  </footer>
+);
+
+export default Footer;
+
+
